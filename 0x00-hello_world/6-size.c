bdf796e703ef3afe2efbab3fc0e737c7a74c764e
ominous-square <laywardwayward@gmail.com>
2023-04-21 07:14:43 +0300

    Change from %lu to %i

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-printf("Size of char:%lu byte(s)\n", sizeof(char));
-printf("Size of int:%lu byte(s)\n", sizeof(int));
-printf("Size of long int:%lu byte(s)\n", sizeof(long int));
-printf("Size of long long int:%lu byte(s)\n", sizeof(long long int));
-printf("Size of float:%lu byte(s)\n", sizeof(float));
-return (0);
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
