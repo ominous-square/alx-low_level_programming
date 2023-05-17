ca1040a9b24295ad96b2a74e1fc848769d3f1cbc
ominous-square <laywardwayward@gmail.com>
2023-05-17 14:10:02 +0300

    Always return file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main: has two arguements
+ * @argc: arguement count
+ * @argv: argguement Vector
+ * return: Always 0
+*/
+
+int main(int argc, char *argv[]){
+    _putchar(argv[0]);
+    _putchar('\n');
+}
