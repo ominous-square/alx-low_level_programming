bb991920fe9075ac9d29569832af8b4cd805df22
ominous-square <laywardwayward@gmail.com>
2023-05-17 15:33:46 +0300

    Returning number or arguemets

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+    _putchar(argc-1);
+    _putchar('\n');
+    return (0);
+}
